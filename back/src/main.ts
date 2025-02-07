a26a894353c6bd90bbca7dc3a08c98cf99bfb102
anselmo <anselmo.vecchio.jose@gmail.com>
2025-02-06 22:08:39 -0300

    Update

--- back/src/main.ts
+++ back/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://chatbot-de-sushi.onrender.com',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
